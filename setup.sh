01ae143188fb65278c532643377152cc1048e685
Gilson Filho <contatogilsonsbf@gmail.com>
2011-11-06 18:00:02 -0200

    Script responsável por fazer o checkout dos plugins e instalar o pep8.vim

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fazendo o checkout dos plugins
+echo "Fazendo o checkout dos plugins e esquemas de cores..."
+sleep 2
+git submodule init
+git submodule update
+
+# Inserindo o pep8.vim
+echo "Fazendo o download do pep8.vim e configurando-o."
+sleep 2
+wget http://www.vim.org/scripts/download_script.php?src_id=14366
+mv download_script.php\?src_id\=14366 pep8.vim
+mv pep8.vim ~/.vim/bundle/pyflakes-vim/ftplugin/python
