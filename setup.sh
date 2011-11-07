f261464792292cfab4bcf6b73116e56f5d367fa2
Gilson Filho <contatogilsonsbf@gmail.com>
2011-11-07 11:09:42 -0200

    Alterando o checkout dos plugins

--- setup.sh
+++ setup.sh
@@ -4,14 +4,7 @@
 echo "Fazendo o checkout dos plugins e esquemas de cores..."
 sleep 2
 git submodule init
-git submodule update
-
-# Configurando pyflakes-vim
-echo "Fazendo o checkout e configuração do pyflakes-vim..."
-sleep 2
-cd ~/.vim/bundle/pyflakes-vim
-git submodule init
-git submodule update
+git submodule update --init --recursive
 
 # Inserindo o pep8.vim
 echo "Fazendo o download do pep8.vim e configurando-o."
@@ -19,5 +12,3 @@ sleep 2
 wget http://www.vim.org/scripts/download_script.php?src_id=14366
 mv download_script.php\?src_id\=14366 pep8.vim
 mv pep8.vim ~/.vim/bundle/pyflakes-vim/ftplugin/python
-echo "O programa GIT não está instalado. Faça a sua instaação."
-exit
